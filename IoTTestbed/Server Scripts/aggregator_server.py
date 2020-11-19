a729002f2c22cdea8ef4e2a282d884831047de46
achara28 <achara28@ucy.ac.cy>
2020-11-19 17:49:49 +0200

    scheduler + aggregator updated

--- IoTTestbed/Server Scripts/aggregator_server.py
+++ IoTTestbed/Server Scripts/aggregator_server.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import asyncore, socket
 import time
-import sys
+
 import pyodbc
 
 cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
@@ -23,7 +23,7 @@ class Server(asyncore.dispatcher):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.bind(('10.16.4.94', port))
-        self.listen(1)
+        self.listen(5)
 
     def handle_accept(self):
         # when we get a client connection start a dispatcher for that
@@ -32,20 +32,20 @@ class Server(asyncore.dispatcher):
         socket, address = self.accept()
         expId = current_experiment_running()
         EchoHandler(socket)
-
 class EchoHandler(asyncore.dispatcher_with_send):
     # dispatcher_with_send extends the basic dispatcher to have an output
     # buffer that it writes whenever there's content
-
     def handle_read(self):
-        ts = time.time()
-        self.out_buffer = self.recv(1024)
-        with open('C:/Users/Andreas/source/repos/IoTTestbed/IoTTestbed/Logs/aggregator_log_' + str(expId) + '.log', 'a') as f:
-            f.write(str(ts)+';'+self.out_buffer)
-
+        self.out_buffer = self.recv(8192)
+        lines = self.out_buffer.split('\n')
+        lines = [x for x in lines if len(x.strip()) > 0]
+        print(len(lines))
+        with open('C:/Users/Andreas/source/repos/IoTTestbed/IoTTestbed/Logs/aggregator_log_' + str(expId) + '.log',
+                  'a+') as f:
+            ts = time.time()
+            for l in lines:
+                f.write(str(ts) + ';' + str(l) + '\n')
         f.close()
-
-
 
 
 s = Server('10.16.4.94', 5007)
--- IoTTestbed/Server Scripts/scheduler.py
+++ IoTTestbed/Server Scripts/scheduler.py
@@ -1,8 +1,5 @@
 import paho.mqtt.client as mqtt
-import subprocess
-import os
 import pyodbc
-import aggregator_client
 
 cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
                       "Server=localhost;"  # DESKTOP-CPEF5HP
@@ -14,64 +11,76 @@ cursor = cnxn.cursor()
 def current_experiment_running():
     cursor.execute("SELECT ExperimentId FROM Experiment WHERE Status='running';")
     row = cursor.fetchone()
-    return row if row is None else row.ExperimentId
+    if row is not None:
+        print('Current experiment running: ' + str(row.ExperimentId))
+    else:
+        print('Current experiment running: ' + str(row))
+
+    return None if row is None else row.ExperimentId
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
+    #client.publish(topic='experiments/next', payload='next', qos=0, retain=False)
+
     client.subscribe("sensors/+/status")  # PIs are publishing in this topic when an experiment is due
 
     client.subscribe("experiments/next")  # Web app is publishing in this topic when a user submit new experiment
-    client.publish(topic='experiments/next', payload='',qos=0,retain=False)
+    client.publish(topic='experiments/next', payload='', qos=0, retain=False)
+
 
 def on_message(client, userdata, msg):
     if str(msg.topic) == 'experiments/next':
         cur_exp = current_experiment_running()
-        print('Current experiment runnning = ', cur_exp)
+
         if cur_exp is None:
             print('No running experiments')
             cursor.execute("SELECT * FROM Experiment WHERE Status='pending' ORDER BY ExperimentId;")
             experiment = cursor.fetchone()
             if experiment is None:
+                print('No scheduled experiments')
                 return
-            next_exp = experiment.ExperimentId
-            next_exp_duration = int(experiment.Duration)
-            # print(next_exp_duration)
-            print('The next experiment that should start is ', next_exp)
-            sensors = []
-            # Publish mqtt message to start the next experiment
-            cursor.execute("SELECT SensorId FROM SensorExperiment WHERE ExperimentId=?;",
-                           next_exp)  # Fetch sensors for the current experiment
+            else:
+                next_exp = experiment.ExperimentId
+                next_exp_duration = int(experiment.Duration)
+                # print(next_exp_duration)
+                print('The next experiment that should start is ', next_exp)
+                sensors = []
+                # Publish mqtt message to start the next experiment
+                cursor.execute("SELECT SensorId FROM SensorExperiment WHERE ExperimentId=?;",
+                               next_exp)  # Fetch sensors for the current experiment
 
-            row = cursor.fetchone()
-
-            while row:
-                sensors.append(row.SensorId)
                 row = cursor.fetchone()
+                while row:
+                    sensors.append(row.SensorId)
+                    row = cursor.fetchone()
 
-            for s in sensors:
-                print s
-                cursor.execute(
-                    "SELECT ProjectName, Filename FROM SensorExperiment WHERE ExperimentId=? AND SensorId=?;",
-                    next_exp, s)
-                row = cursor.fetchone()
-                client.publish('sensors/' + str(s), '/home/pi/contikiFirmwares/experiments/' + str(
-                    next_exp) + '/' + row.ProjectName + '/' + row.Filename + " " + str(next_exp_duration))
+                for s in sensors:
+                    print s
+                    cursor.execute(
+                        "SELECT ProjectName, Filename FROM SensorExperiment WHERE ExperimentId=? AND SensorId=?;",
+                        next_exp, s)
+                    row = cursor.fetchone()
+                    cursor.execute("UPDATE Sensor SET Status='active' WHERE SensorId=?;", s)
+                    cnxn.commit()
+                    client.publish('sensors/' + str(s), '/home/pi/contikiFirmwares/experiments/' + str(
+                        next_exp) + '/' + row.ProjectName + '/' + row.Filename + " " + str(next_exp_duration))
 
-            print('Sensors for current experiment ', sensors)
+                print('Sensors for current experiment ', sensors)
 
-            cursor.execute("UPDATE Experiment SET Status='running' WHERE ExperimentId=?;", next_exp)
-            cnxn.commit()
+                cursor.execute("UPDATE Experiment SET Status='running' WHERE ExperimentId=?;", next_exp)
+                cnxn.commit()
 
         # Schedule experiment i.e Set Experiment Status to pending
         if cur_exp is not None:
-            print(cur_exp)
-            cursor.execute("UPDATE Experiment SET Status='pending' WHERE ExperimentId=?;", msg.payload)
+            print('Current experiment runnning = ', cur_exp)
+            print(str(cur_exp) + " must be scheduled")
+            cursor.execute("UPDATE Experiment SET Status='pending' WHERE ExperimentId=?;", int(msg.payload))
             cnxn.commit()
 
-    if str(msg.topic).split('/')[0] == 'sensors':
+    elif str(msg.topic).split('/')[0] == 'sensors':
         print('wtfs')
         cur_exp = current_experiment_running()  # continue here
         print(msg.topic + " " + str(msg.payload))
@@ -82,15 +91,15 @@ def on_message(client, userdata, msg):
         # this must be changed if we want to execute simultaneous experiments in different sensors.
         # In this i check whether all sensors are available meaning that experiment is finished.
         cursor.execute("SELECT * FROM Sensor Where Status='active';")
-        print(cursor.fetchone())
+        # print(cursor.fetchone())
         if cursor.fetchone() is None:  # No running sensors, experiment is finished
-            print('Gets in here')
-            cursor.execute("UPDATE Experiment SET Status='finished' WHERE ExperimentId=?;", cur_exp)     #change this to Status=finished
+            print('Experiment is finished')
+            cursor.execute("UPDATE Experiment SET Status='finished' WHERE ExperimentId=?;",
+                           cur_exp)  # change this to Status=finished
             cnxn.commit()
             cursor.execute("UPDATE Experiment SET Log=? WHERE ExperimentId=?;",
                            'C:/Users/Andreas/source/repos/IoTTestbed/IoTTestbed/Logs/aggregator_log_' + str(
                                cur_exp) + '.log', cur_exp)
-
 
             cnxn.commit()
             # Start the scheduled experiments according to queue (oldest id and pending)
